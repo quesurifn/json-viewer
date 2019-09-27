aa29078c3be27723f34fec151788a889de6135f8
quesurifn <kyle.c.r.fahey@gmail.com>
2019-09-27 18:49:21 -0500

    Add build

---
+++ build/precache-manifest.6c240f262820cdc2b4e50675abbf4ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e22690c75f5b7a1294b9a7f168b3b28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d499bed5ffcda3c712b",
+    "url": "/static/css/2.fc4895e3.chunk.css"
+  },
+  {
+    "revision": "c8caddcce2c7890d841e",
+    "url": "/static/css/main.c481372a.chunk.css"
+  },
+  {
+    "revision": "0d499bed5ffcda3c712b",
+    "url": "/static/js/2.dc893a43.chunk.js"
+  },
+  {
+    "revision": "c8caddcce2c7890d841e",
+    "url": "/static/js/main.35dcd2f5.chunk.js"
+  },
+  {
+    "revision": "acb5ac132a0b5585e02f",
+    "url": "/static/js/runtime-main.fb338dfc.js"
+  },
+  {
+    "revision": "5d4b2ab775cefacd4240dd5dcf2c7f60",
+    "url": "/static/media/logo.5d4b2ab7.png"
+  }
+]);
